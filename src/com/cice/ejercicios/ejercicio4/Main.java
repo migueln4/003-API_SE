36df10297e8ac7918f302ae77c843e837c28a9eb
Miguel <miguel@11062018>
2018-06-24 20:13:19 +0200

    Hecho el ejercicio de sacar los espacios y poner las barras.

---
+++ src/com/cice/ejercicios/ejercicio4/Main.java
@@ -0,0 +1,31 @@
+package com.cice.ejercicios.ejercicio4;
+import static java.lang.Math.*;
+import java.security.SecureRandom;
+
+//Crear un programa que muestre la suma de todos los números enteros comprendidos entre dos números aleatorios generados por el propio programa. Los números aleatorios deben ser mayores sde 0 y menores de 100.
+
+public class Main {
+    public static void main(String[] args) {
+        int a = (int) round(random()*100);
+        int b = (int) round(random()*100);
+        System.out.println(a+"\t"+b);
+        int suma = 0;
+        for (int i=min(a,b);i<=max(a,b);i++) {
+            suma += i;
+        }
+        System.out.println(suma);
+        alternativa();
+    }
+
+    private static void alternativa() {
+        SecureRandom sr = new SecureRandom();
+        int a = sr.nextInt(101);
+        int b = sr.nextInt(101);
+        System.out.println(a+"\t"+b);
+        int suma = 0;
+        for (int i=min(a,b);i<=max(a,b);i++) {
+            suma += i;
+        }
+        System.out.println(suma);
+    }
+}
